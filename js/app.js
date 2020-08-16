2072266dfd1c9a355c899563f0075aaf56891517
Olek Kalashnyk <olek.kalashnyk@gmail.com>
2020-08-16 02:12:23 +0200

    firsr lesson

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 "use strict";
 
-const y = false;
+const numberOfFilms = prompt("How many films you see?", "");
 
-console.log(!y);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const 
+    a = prompt("Last see film?", ""),
+    b = prompt("How many star???", ""),
+    c = prompt("Last see film?", ""),
+    d = prompt("How many star???", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
