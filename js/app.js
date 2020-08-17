c0bacf4aeb952c1da97ed101112985b64fe53af2
Olek Kalashnyk <olek.kalashnyk@gmail.com>
2020-08-17 14:27:49 +0200

    Add skill vs While / doWhile / for

--- js/app.js
+++ js/app.js
@@ -1,2 +1,27 @@
 "use strict";
-//While 
+//while(){}
+
+// let num = 50;
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+
+//do{}while()
+
+// let num = 50;
+// do {
+//     console.log(num);
+//     num++;
+// } while (num <= 55);
+
+
+//for(let i=0;Uslowie;Shag cykla)
+
+for(let i = 0; i<=10; i++){
+    if(i==9){break;}
+    if(i==3 || i == 5){continue;}
+    console.log(i);
+    
+}
