ed29dfa71eb7be7a1d4699b6d09a05a89b1ce0b0
Olek Kalashnyk <olek.kalashnyk@gmail.com>
2020-08-17 13:40:09 +0200

    Start lean  While

--- js/app.js
+++ js/app.js
@@ -1,22 +1,2 @@
 "use strict";
-
-const numberOfFilms = prompt("How many films you see?", "");
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const 
-    a = prompt("Last see film?", ""),
-    b = prompt("How many star???", ""),
-    c = prompt("Last see film?", ""),
-    d = prompt("How many star???", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+//While 
