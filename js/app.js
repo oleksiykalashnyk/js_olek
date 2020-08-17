791941e12a687615ed3c9e3c752f1e0ec7107592
Olek Kalashnyk <olek.kalashnyk@gmail.com>
2020-08-17 13:18:43 +0200

    If/Else

--- js/app.js
+++ js/app.js
@@ -1,22 +1,7 @@
 "use strict";
 
-const numberOfFilms = prompt("How many films you see?", "");
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const 
-    a = prompt("Last see film?", ""),
-    b = prompt("How many star???", ""),
-    c = prompt("Last see film?", ""),
-    d = prompt("How many star???", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+if (4 == 5) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
