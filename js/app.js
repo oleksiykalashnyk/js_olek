cf208aed72b6867d4362ccabbc3a949dbfd72739
OleksiyKalashnyk <olek.kalashnyk@gmail.com>
2020-08-18 02:20:39 +0200

    a

--- js/app.js
+++ js/app.js
@@ -9,3 +9,25 @@ function learnJS(lang, callback) {
 learnJS('JavaScript', function () {
     console.log("Я прошёл этот урок");
 })
+
+
+//Next gen CallBack function
+
+function x(num, func) {
+    console.log("1");
+    console.log(num);
+    func(num);
+}
+
+function y() {
+    console.log("3");
+}
+
+function z(intInFunctionX) {
+    console.log("Summa = " + "4" + +intInFunctionX);
+}
+
+
+x(3, y);
+
+x("2", z);
