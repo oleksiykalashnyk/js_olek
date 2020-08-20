5a0953dc31171bcbdb9c1fc4f6e783d1c5544422
OleksiyKalashnyk <olek.kalashnyk@gmail.com>
2020-08-21 00:21:13 +0200

    Finish firsh part OOP in JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 "use strict";
 //Learn Object-oriented programming (OOP)
 
+//Старый способ прототипирования через __proto__
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: () => "Hello"
+};
+
+const jonh = {
+    health: 100
+};
+jonh.__proto__ = soldier;
+console.log(jonh.sayHello());
+
+//Новый способ - который еквивалентный первому 
+const olek = {};
+Object.setPrototypeOf(olek,soldier);
+console.log(olek.sayHello());
+
+
+//Более Юзабельный способ и более качественный
+const maxim = Object.create(olek);
+console.log(maxim.armor);
+console.log(maxim.health);
