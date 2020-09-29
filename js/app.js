dbd0981b9b3ec4687702a46e826c4beb5cb24ebb
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2020-09-29 21:29:38 +0200

    finish learn this lesson

--- js/app.js
+++ js/app.js
@@ -1,60 +1,17 @@
 "use strict";
-//Start work for DOM Elements in the Page 
+//Start work for async, defer dynamic scripts 
 
-//1. body
-
-console.log(document.body);
-
-//2. All NODE childs inside body
-console.log(document.body.childNodes);
-
-//2.1 First NODE childs inside body
-console.log(document.body.firstChild);
-
-//2.1 Last NODE childs inside body
-console.log(document.body.lastChild);
-
-//3 Get the parent NODE element
-console.log(document.querySelector('#current').parentNode);
-
-//4. Use data-* attribute
-console.log(document.querySelector('[data-current="3"]'));
-
-//5. Use next NODE element
-console.log(document.querySelector('[data-current="3"]').nextSibling);
+const p = document.querySelectorAll('p');
+console.log(p);
 
 
-//6. Use previous NODE element
-console.log(document.querySelector('[data-current="3"]').previousSibling);
-
-////////////////////////////////////
-//7. Use ELEMENT
-console.log("///////////////////////");
-
-
-//7.1 Get the parent element
-console.log(document.querySelector('[data-current="3"]').parentElement);
-
-//7.2 First child inside body
-console.log(document.body.firstElementChild);
-
-//7.3 Last child inside body
-console.log(document.body.lastElementChild);
-
-//7.4 Use previous element
-console.log(document.querySelector('[data-current="3"]').previousElementSibling);
-
-//7.5 Use next element
-console.log(document.querySelector('[data-current="3"]').nextElementSibling);
-
-////////////////////////////////////
-// 8. Use FOR/OF
-console.log("///////////////////////");
-////////////////////////////////////
-
-for (let node of document.body.childNodes) {
-    if (node.nodeName == '#text') {
-        continue;
-    }
-    console.log(node);
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    script.defer = true;
+    document.body.append(script);
 }
+
+loadScript("js/test.js");
+loadScript("js/some.js");
