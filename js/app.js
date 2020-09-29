b813f3358a9473767193e49444d0c5f42d7d0686
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2020-09-29 19:01:55 +0200

    finish learn DOM

--- js/app.js
+++ js/app.js
@@ -1,77 +1,60 @@
 "use strict";
-//Start work for Elements in the Page 
+//Start work for DOM Elements in the Page 
 
-//1. set event in HTML  - Not recommended
-// <button onclick="alert('Hi im button 1')">1</button>
+//1. body
 
-//2. set event in JS  - Not recommended too
-const btn2 = document.querySelector("#button_2");
+console.log(document.body);
 
-btn2.onclick = function () {
-    alert('Hi im button 2');
-};
+//2. All NODE childs inside body
+console.log(document.body.childNodes);
 
-//3. set event Listener in JS use array function - Recommended 
-const btn3 = document.querySelector("#button_3");
+//2.1 First NODE childs inside body
+console.log(document.body.firstChild);
 
-btn3.addEventListener("click", (i) => {
-    // i this event
-    console.log(i.target);
-    alert('You click  button 3');
-});
+//2.1 Last NODE childs inside body
+console.log(document.body.lastChild);
 
-//4. set event Listener in JS use normal callBack function - Recommended 
-const btn4 = document.querySelector("#button_4");
-let num = 0;
+//3 Get the parent NODE element
+console.log(document.querySelector('#current').parentNode);
 
-const alertThisButton4 = (i) => {
-    // console.log(i.target);
-    alert('You click  button 4');
-    num++;
-    if (num == 1) {
-        btn4.removeEventListener("click", alertThisButton4);
+//4. Use data-* attribute
+console.log(document.querySelector('[data-current="3"]'));
+
+//5. Use next NODE element
+console.log(document.querySelector('[data-current="3"]').nextSibling);
+
+
+//6. Use previous NODE element
+console.log(document.querySelector('[data-current="3"]').previousSibling);
+
+////////////////////////////////////
+//7. Use ELEMENT
+console.log("///////////////////////");
+
+
+//7.1 Get the parent element
+console.log(document.querySelector('[data-current="3"]').parentElement);
+
+//7.2 First child inside body
+console.log(document.body.firstElementChild);
+
+//7.3 Last child inside body
+console.log(document.body.lastElementChild);
+
+//7.4 Use previous element
+console.log(document.querySelector('[data-current="3"]').previousElementSibling);
+
+//7.5 Use next element
+console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+////////////////////////////////////
+// 8. Use FOR/OF
+console.log("///////////////////////");
+////////////////////////////////////
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
     }
-};
-
-btn4.addEventListener("click", alertThisButton4);
-
-
-//5. set event Listener in JS use normal callBack function 
-//And use overlay click currentTarget
-const btn5 = document.querySelector("#button_5");
-const overlay = document.querySelector(".overlay");
-
-let num5 = 0;
-
-const alertThisButton5 = (i) => {
-    console.log(i.currentTarget);
-    console.log(i.type);
-    alert('You click  button 5');
-    num5++;
-    if (num5 == 1) {
-        btn5.removeEventListener("click", alertThisButton5);
-    }
-};
-
-btn5.addEventListener("click", alertThisButton5);
-overlay.addEventListener("click", alertThisButton5);
-
-
-//6. set event for LINK  -> Dont go for Link
-
-const link = document.querySelector("a");
-
-link.addEventListener("click", event => {
-    event.preventDefault();
-    console.log(event.target);
-});
-
-
-//7. event options - once -> only one Event 
-
-link.addEventListener("click", event => {
-    event.preventDefault();
-    console.log("One click");
-}, {
-    once: true
-});
+    console.log(node);
+}
